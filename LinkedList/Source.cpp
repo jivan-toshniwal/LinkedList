5946e5ea8064457e2ca364a6f8a3d5f2aed8bf13
JIVAN TOSHNIWAL <j.toshniwal@yahoo.co.in>
2021-07-15 12:07:54 +0530

    Added

--- LinkedList/Source.cpp
+++ LinkedList/Source.cpp
@@ -2,149 +2,174 @@
 
 class Node {
 	int data;
-	Node* next = NULL;
+	Node* next, * last;
 public:
-	Node(int n)
-		:data(n) {}
-
-	void creatLinkList(Node*& head, int val);
-	void display(Node* head);
-	void missing_num(Node* head);
-	void insert(Node*& head, int after, int num);
-	void delete_num(Node*& head, int x);
-	void reverse_link(Node*& head);
-	void recurciveDisplay(Node*& head);
-	Node* recursiveReverse(Node*& head);
-};
-
-void Node::recurciveDisplay(Node*& head) {
-	if (head != NULL) {
-		std::cout << head->data << " ";
-		recurciveDisplay(head->next);
+	Node* head;
+	Node(int val = 0)
+		:data(val) {
+		head = next = last = NULL;
 	}
-}
 
-void Node::creatLinkList(Node*& head, int val) {
-	Node* n = new Node(val);
-	if (head == NULL) {
-		head = n;
-		return;
-	}
-	else {
-		Node* temp = head;
-		while (temp->next != NULL)
+	void createList() {
+		std::cout << "Enter size of List: ";
+		int x; std::cin >> x;
+		while (x--)
 		{
-			temp = temp->next;
+			int y; std::cin >> y;
+			Node* n = new Node(y);
+			if (!head) {
+				head = last = n;
+			}
+			else {
+				last->next = n;
+				last = last->next;
+			}
+		}std::cout << std::endl;
+	}
+	void display(Node* h)
+	{
+		while (h)
+		{
+			std::cout << h->data << " ";
+			h = h->next;
+		}std::cout << std::endl;
+	}
+	void insertAtlast(int val) {
+		Node* n = new Node(val);
+		last->next = n;
+		last = last->next;
+	}
+	void recDisplay(Node* h) {
+		if (h) {
+			std::cout << h->data << " ";
+			recDisplay(h->next);
 		}
-		temp->next = n;
 	}
-}
-
-void Node::reverse_link(Node*& head) {
-	Node* prev = NULL;
-	Node* prev_plus = NULL;
-	Node* curr = head;
-	while (curr != NULL) {
-		prev = prev_plus;
-		prev_plus = curr;
-		curr = curr->next;
-		prev_plus->next = prev;
+	void countNode(Node* h) {
+		int x = 0;
+		while (h)
+		{
+			x++;
+			h = h->next;
+		}
+		std::cout << "No of Nodes are: " << x << std::endl;
 	}
-	head = prev_plus;
-}
-
-Node* Node::recursiveReverse(Node*& head) {
-	if (head == NULL || head->next == NULL) {
-		return head;
+	int sum(Node* h) {
+		if (!h->next)
+		{
+			return h->data;
+		}
+		return h->data + sum(h->next);
 	}
-	Node* NewList = recursiveReverse(head->next);
-	head->next->next = head;
-	head->next = NULL;
-	return NewList;
-}
-
-void Node::display(Node* head) {
-	std::cout << std::endl;
-	while (head != NULL)
-	{
-		std::cout << head->data << " ";
-		head = head->next;
+	int max_ele(Node* h) {
+		int max = INT_MIN;
+		while (h)
+		{
+			max = (max > h->data) ? max : h->data;
+			h = h->next;
+		}
+		return max;
 	}
-}
-
-void Node::missing_num(Node* head) {
-	int x = head->data;
-	int k = 1;
-	while (head != NULL)
-	{
-		if (x != head->data) {
-			std::cout << k++ << " st Missing Number is: " << x++ << std::endl;
+	bool search_Node(Node* h, int num) {
+		if(!h){
+			return false;
+		}
+		else if (h->data == num) {
+			return true;
+		}
+		search_Node(h->next, num);
+	}
+	void insert_Node_pos(Node*& h, int val, int pos) {
+		Node* n = new Node(val);
+		if (!pos) {
+			n->next = h;
+			h = n;
 		}
 		else {
+			Node* temp = h;
+			while (--pos)
+			{
+				if (temp->next) {
+					temp = temp->next;
+				}
+				else {
+					std::cout << "Position out of range!!!\n";
+					return;
+				}
+			}
+			n->next = temp->next;
+			temp->next = n;
+		}
+	}
+	void deleteNode(int pos)
+	{
+		if (!pos) {
+			Node* to_del = head;
 			head = head->next;
-			x++;
+			to_del->next = NULL;
+			delete to_del;
 		}
-
-	}
-}
-
-void Node::insert(Node*& head, int after, int num) {
-	Node* n = new Node(num);
-	if (head == NULL) {
-		head = n;
-		return;
-	}
-	if (after == 0) {
-		Node* temp = head;
-		while (temp->next != NULL)
-		{
-			temp = temp->next;
+		else {
+			Node* temp = head;
+			while (--pos)
+			{
+				if (temp->next) {
+					temp = temp->next;
+				}
+				else {
+					std::cout << "No Node Present to delete at this pos!!\n";
+					return;
+				}
+			}
+			if (temp->next) {
+				Node* to_del = temp->next;
+				temp->next = to_del->next;
+				delete to_del;
+			}
+			else {
+				std::cout << "No Node Present to delete at this pos!!\n";
+				return;
+			}
 		}
-		temp->next = n;
-		return;
 	}
-	Node* temp = head;
-	while (temp->data != after)
-	{
-		temp = temp->next;
+	Node* insertAtBegining(Node* h, int x) {
+		Node* n = new Node(x);
+		n->next = h;
+		h = n;
+		return h;
 	}
-	Node* temp2 = temp->next;
-	temp->next = n;
-	temp->next->next = temp2;
-}
-
-void Node::delete_num(Node*& head, int x) {
-	Node* temp = head;
-	while (temp->next->data != x)
-	{
-		temp = temp->next;
+	Node* insertAtEnd(Node* head, int x) {
+		Node* n = new Node(x);
+		if (!head) {
+			return n;
+		}
+		else {
+			Node* temp = head;
+			while (temp->next) {
+				temp = temp->next;
+			}
+			temp->next = n;
+			return head;
+		}
 	}
-	Node* to_delete = temp->next;
-	Node* n = temp->next->next;
-	temp->next = n;
-	delete to_delete;
-}
+	bool is_sorted(Node*& h) {
+		if (!h || !h->next) {
+			return true;
+		}
+		else {
+			return ((h->data < h->next->data) && is_sorted(h->next));
+		}
+	}
+};
 
 int main()
 {
-	Node* head = NULL;
-	int x;
-	std::cout << "How many items you want to add: ";
-	std::cin >> x;
-	std::cout << "\n\nPlease add " << x << " items with space: ";
-	for (int i = 0; i < x; i++) {
-		int y;
-		std::cin >> y;
-		head->creatLinkList(head, y);
-	}
-	std::cout << std::endl;
-	/*head->display(head);
-	head->missing_num(head);
-	head->insert(head, 0, 6);
-	head->display(head);
-	head->delete_num(head, 3);
-	head->display(head);*/
-	//head->reverse_link(head);
-	Node* rhead = head->recursiveReverse(head);
-	head->recurciveDisplay(rhead);
+	Node n;
+	n.createList();
+	n.display(n.head);
+	n.deleteNode(6);
+	n.display(n.head);
+
+	std::cin.get();
+	return 0;
 }
